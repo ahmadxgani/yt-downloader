2fba40310bd932aa63e401eea7d6824da11d52bc
QUERY-GANI <ahmadxgani01@gmail.com>
2021-07-07 18:18:58 +0700

    fix permission bug

--- bin/ytdl.js
+++ bin/ytdl.js
@@ -4,6 +4,7 @@ const config = require("../env.json")
 
 if (process.argv.length === 4 && process.argv[2] === "--setDir") {
     config.baseDir = process.argv[3]
+    Fs.chmod("../env.json", "777", () => {})
     Fs.writeFileSync("../env.json", JSON.stringify(config))
 } else {
     const { start } = require("../src/index")
