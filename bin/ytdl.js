7ec0d1bb7fcdc21a2861a69ac079da62c8b7fca7
QUERY-GANI <ahmadxgani01@gmail.com>
2021-07-07 18:34:40 +0700

    fix

--- bin/ytdl.js
+++ bin/ytdl.js
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
 const Fs = require("fs")
 const config = require("../env.json")
+const path = require("path")
+const envFile = path.join(process.env.HOME, "env.json")
 
 if (process.argv.length === 4 && process.argv[2] === "--setDir") {
     config.baseDir = process.argv[3]
-    Fs.chmod("../env.json", "777", () => {})
-    Fs.writeFileSync("../env.json", JSON.stringify(config))
+    Fs.writeFileSync(envFile, JSON.stringify(config))
 } else {
+    if (!Fs.existsSync(envFile)) Fs.writeFileSync(envFile, JSON.stringify({ baseDir: "downloads" }))
     const { start } = require("../src/index")
     start()
 }
