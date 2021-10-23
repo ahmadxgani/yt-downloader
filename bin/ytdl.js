7534ff4a0436f507afcacdd4b6d840a7afaa126f
QUERY-GANI <ahmadxgani01@gmail.com>
2021-10-23 17:17:16 +0700

    refactoring

--- bin/ytdl.js
+++ bin/ytdl.js
@@ -3,16 +3,16 @@ const path = require("path")
 const envFile = path.join(process.env.HOME, "env.json")
 const Fs = require("fs")
 
-if (!Fs.existsSync(envFile)) Fs.writeFileSync(envFile, JSON.stringify({ baseDir: "downloads" }))
+if (!Fs.existsSync(envFile)) Fs.writeFileSync(envFile, JSON.stringify({ baseDir: "Downloads" }))
 
 const { start, retry } = require("../src/index")
 
 if (process.argv.length === 4 && process.argv[2] === "--setDir") {
-    const config = require(envFile)
-    config.baseDir = process.argv[3]
-    Fs.writeFileSync(envFile, JSON.stringify(config))
+  const config = require(envFile)
+  config.baseDir = process.argv[3]
+  Fs.writeFileSync(envFile, JSON.stringify(config))
 } else if (process.argv.length === 3 && process.argv[2] === "--retry") {
-    retry()
+  retry()
 } else {
-    start()
+  start()
 }
