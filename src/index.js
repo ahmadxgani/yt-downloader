37339d5e224c8eff15821a7400f654d43f76e830
lelouch <lelouch@ll.ll.my.id>
2022-05-28 10:32:20 +0700

    change download method

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const axios = require("axios").default
 const cheerio = require("cheerio")
+const download = require("download")
 const Fs = require("fs")
 const { Listr } = require("listr2")
 const path = require("path")
@@ -102,7 +103,8 @@ const tasks = new Listr(
       task: async (ctx, task) => {
         task.title = `save video to "${path.join(process.env.HOME, config.baseDir, ctx.input[output[1]])}" folder`
         task.output = `total videos: ${ctx.linkVideos.length}`
-        ctx.log = await download(ctx.linkVideos, ctx.input[output[1]])
+        await downloads(ctx.linkVideos, ctx.input[output[1]])
+        // ctx.log = await downloads(ctx.linkVideos, ctx.input[output[1]])
       },
       options: {
         persistentOutput: true,
@@ -114,10 +116,11 @@ const tasks = new Listr(
 
 exports.start = async () => {
   try {
-    const ctx = await tasks.run()
-    Fs.writeFileSync(path.join(process.env.HOME, "ytdl.json"), JSON.stringify(ctx.log))
-    const failedVideo = ctx.log.filter((val) => val.isFailed)
-    if (failedVideo.length) console.log(`Done with ${failedVideo.length} failed file, you can visit the log on "${path.join(process.env.HOME, "ytdl.json")}" or retry download with command "ytdl --retry"`)
+    await tasks.run()
+    // const ctx = await tasks.run()
+    // Fs.writeFileSync(path.join(process.env.HOME, "ytdl.json"), JSON.stringify(ctx.log))
+    // const failedVideo = ctx.log.filter((val) => val.isFailed)
+    // if (failedVideo.length) console.log(`Done with ${failedVideo.length} failed file, you can visit the log on "${path.join(process.env.HOME, "ytdl.json")}" or retry download with command "ytdl --retry"`)
   } catch (e) {
     console.error(e)
   }
@@ -134,7 +137,9 @@ exports.retry = async () => {
       const dirName = path.join(failVideo.fullPath, "..")
       const arr = failVideo.fullPath.split("/")
       const fileName = arr[arr.length - 1]
-      await save(dirName, fileName, failVideo.fullPath, failVideo.link)
+      // hapus item yg gagal jika file berhasil / sudah selesai di download dan di simpan ( belum terlaksana )
+      await downloads(failVideo.link, failVideo.fullPath)
+      // await save(dirName, fileName, failVideo.fullPath, failVideo.link)
     })
   } catch (e) {
     console.log(e.message)
@@ -248,28 +253,40 @@ const getSingleVid = ({ v_id, defaultResolution, resolution }) => {
   })
 }
 
-const download = (videos, dirName) => {
+const downloads = async (videos, dirName) => {
   dirName = path.join(process.env.HOME, config.baseDir, dirName)
-  const log = videos.map(async (video) => {
-    const fileName = video.fileName.replace(/\//g, " of ")
-    const output = path.join(dirName, fileName)
-    try {
-      await save(dirName, fileName, output, video.downloadLink)
-      return {
-        link: video.downloadLink,
-        fullPath: output,
-        isFailed: false,
+  await Promise.all(
+    videos.map((video) => {
+      const fileName = video.fileName.replace(/\//g, " of ")
+      const output = path.join(dirName, fileName)
+      if (video.downloadLink) {
+        download(video.downloadLink, output)
       }
-    } catch {
-      console.log(`video with "${fileName}" name failed to save`)
-      return {
-        link: video.downloadLink,
-        fullPath: output,
-        isFailed: true,
-      }
-    }
-  })
-  return Promise.all(log)
+    })
+  )
+  console.log("video downloaded")
+
+  // const log = videos.map(async (video) => {
+  //   const fileName = video.fileName.replace(/\//g, " of ")
+  //   const output = path.join(dirName, fileName)
+  //   try {
+
+  //     await save(dirName, fileName, output, video.downloadLink)
+  //     return {
+  //       link: video.downloadLink,
+  //       fullPath: output,
+  //       isFailed: false,
+  //     }
+  //   } catch {
+  //     console.log(`video with "${fileName}" name failed to save`)
+  //     return {
+  //       link: video.downloadLink,
+  //       fullPath: output,
+  //       isFailed: true,
+  //     }
+  //   }
+  // })
+  // return Promise.all(log)
 }
 
 const save = async (dirName, fileName, output, downloadLink) => {
